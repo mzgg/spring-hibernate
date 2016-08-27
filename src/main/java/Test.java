a1ac3659d8afc76736d4d64963c02034f9b1b6e7
mzgg <mehmetzahitt@gmail.com>
2016-08-27 10:25:18 +0300

    created model object and main class

---
+++ src/main/java/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by GUNEY on 27.08.2016.
+ */
+public class Test {
+}
---
+++ src/main/java/com/mehmetzahit/model/Person.java
@@ -0,0 +1,34 @@
+package com.mehmetzahit.model;
+
+/**
+ * Created by GUNEY on 27.08.2016.
+ */
+public class Person {
+    private int id;
+    private String name;
+    private String surname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
