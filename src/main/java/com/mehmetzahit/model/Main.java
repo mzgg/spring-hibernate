d73cb0e5a9f3d23386900f05d644b6f66ebbaf05
mzgg <mehmetzahitt@gmail.com>
2016-08-31 10:54:39 +0300

    trying conflict file

---
+++ src/main/java/com/mehmetzahit/model/Main.java
@@ -0,0 +1,16 @@
+package com.mehmetzahit.model;
+
+/**
+ * Created by GUNEY on 31.08.2016.
+ */
+public class Main {
+
+    public int add(int x, int y) {
+
+        return x * y;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
