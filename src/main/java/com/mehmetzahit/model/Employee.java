2445f15d0bfbb0d0e2cda2ce3fb9a8b25b75f7e4
mzguney <mzguney@netas.com.tr>
2017-09-08 15:12:28 +0300

    feature/NETM-1242

--- src/main/java/com/mehmetzahit/model/Employee.java
+++ src/main/java/com/mehmetzahit/model/Employee.java
@@ -7,5 +7,8 @@ public class Employee {
 
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
     }
 }
